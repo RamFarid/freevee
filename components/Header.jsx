32cef0280444dbe90c98d44add323fb39503c1cc
RamFarid <workprojects22@gmail.com>
2023-04-03 02:04:08 +0200

    Add my social links

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,6 +10,7 @@ import FacebookIcon from '@mui/icons-material/Facebook'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import React from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 
 const styles = {
   haeder: {
@@ -30,14 +31,27 @@ function Header() {
           Freevee
         </Typography>
         <Stack direction={'row'}>
-          <IconButton size='small'>
+          <IconButton
+            target='_blank'
+            size='small'
+            color='primary'
+            href='https://www.facebook.com/rraaamm_s'
+          >
             <FacebookIcon />
           </IconButton>
-          <IconButton size='small'>
-            <TwitterIcon />
+          <IconButton
+            target='_blank'
+            size='small'
+            href='https://api.whatsapp.com/send?phone=201553706448&text=Welcome%20from%20FREEVEE'
+          >
+            <WhatsAppIcon sx={{ color: '#25D366' }} />
           </IconButton>
-          <IconButton size='small'>
-            <GitHubIcon />
+          <IconButton
+            size='small'
+            href='https://github.com/RamFarid'
+            target='_blank'
+          >
+            <GitHubIcon sx={{ color: '#000' }} />
           </IconButton>
         </Stack>
       </Toolbar>
