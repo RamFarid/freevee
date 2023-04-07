79f30177bd1e16c392a65cd4f1d764e32c07c050
RamFarid <workprojects22@gmail.com>
2023-04-07 17:41:44 +0200

    Refactor /api/video endpoint

--- pages/api/video/index.js
+++ pages/api/video/index.js
@@ -1,51 +1,53 @@
-import ytdl from 'youtube-dl-exec'
-import mime from 'mime'
 export default async function handler(req, res) {
-  const { videoLink, itag } = req.query
+  res.status(503).json({
+    code: 500,
+    message: 'Coming soon!',
+  })
+  // const { videoLink, itag } = req.query
 
-  console.log(videoLink)
-  console.log(itag)
-  if (!videoLink || !itag) {
-    res.setHeader('Content-Type', 'application/json')
-    res.status(400).json({
-      code: 400,
-      message: 'Bad request: the video link is missing.',
-    })
-    return
-  }
-  try {
-    const videoInfo = await ytdl(videoLink, {
-      dumpSingleJson: true,
-      noWarnings: true,
-      preferFreeFormats: true,
-    })
-    console.log('first')
-    const format = videoInfo.formats.find((el) => el.format_id == itag)
-    console.log('Second')
-    const videoReadStream = ytdl(videoLink, [
-      'youtube-dl',
-      '-f',
-      itag,
-      '-o',
-      '-',
-      videoLink,
-    ])
-    console.log('Third')
-    const MIME_TYPE = mime.getType(format.ext)
-    console.log('Fourth')
-    res.setHeader(
-      'Content-Disposition',
-      `attachment; filename="${videoInfo.title}.${format.ext}"`
-    )
-    console.log('fifth')
-    res.setHeader('Content-Type', MIME_TYPE)
-    console.log('Sixth')
-    videoReadStream.pipe(res)
-    console.log('Seventh')
-  } catch (error) {
-    res.setHeader('Content-Type', 'application/json')
-    res
-      .status(500)
-      .json({ code: 500, message: "Couldn't get video stream", error })
-  }
+  // console.log(videoLink)
+  // console.log(itag)
+  // if (!videoLink || !itag) {
+  //   res.setHeader('Content-Type', 'application/json')
+  //   res.status(400).json({
+  //     code: 400,
+  //     message: 'Bad request: the video link is missing.',
+  //   })
+  //   return
+  // }
+  // try {
+  //   const videoInfo = await ytdl(videoLink, {
+  //     dumpSingleJson: true,
+  //     noWarnings: true,
+  //     preferFreeFormats: true,
+  //   })
+  //   console.log('first')
+  //   const format = videoInfo.formats.find((el) => el.format_id == itag)
+  //   console.log('Second')
+  //   const videoReadStream = ytdl(videoLink, [
+  //     'youtube-dl',
+  //     '-f',
+  //     itag,
+  //     '-o',
+  //     '-',
+  //     videoLink,
+  //   ])
+  //   console.log('Third')
+  //   const MIME_TYPE = mime.getType(format.ext)
+  //   console.log('Fourth')
+  //   res.setHeader(
+  //     'Content-Disposition',
+  //     `attachment; filename="${videoInfo.title}.${format.ext}"`
+  //   )
+  //   console.log('fifth')
+  //   res.setHeader('Content-Type', MIME_TYPE)
+  //   console.log('Sixth')
+  //   videoReadStream.pipe(res)
+  //   console.log('Seventh')
+  // } catch (error) {
+  //   res.setHeader('Content-Type', 'application/json')
+  //   res
+  //     .status(500)
+  //     .json({ code: 500, message: "Couldn't get video stream", error })
+  // }
 }
